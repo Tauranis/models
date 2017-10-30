9e9fd8ea9c27ec32a93e987c4313c33beda6e453
Rodrigo de Freitas Pereira <rodrigofp@ciandt.com>
2017-10-30 18:05:37 -0200

    Update the size of train and validation set

--- slim/datasets/cdiscounts.py
+++ slim/datasets/cdiscounts.py
@@ -30,11 +30,14 @@ from datasets import dataset_utils
 slim = tf.contrib.slim
 
 #_FILE_PATTERN = 'cdiscount_%s_*.tfrecord'
-_FILE_PATTERN = 'cdiscount_%s*.tfrecord.gz'
+#_FILE_PATTERN = 'cdiscount_%s*.tfrecord.gz'
+_FILE_PATTERN = '%s*.tfrecord.gz'
 
-SPLITS_TO_SIZES = {'train': 110, 'validation': 10} # TODO: ALTERAR
+SPLITS_TO_SIZES = {'train': 11084378, 'validation': 1286915}  # TODO: ALTERAR
+# train: 11084378
+# validation: 1286915
 
-_NUM_CLASSES = 36#5270 # Alterado
+_NUM_CLASSES = 5270  # Alterados
 
 _ITEMS_TO_DESCRIPTIONS = {
     'image': 'A RGB image.',
@@ -43,59 +46,58 @@ _ITEMS_TO_DESCRIPTIONS = {
 
 
 def get_split(split_name, dataset_dir, file_pattern=None, reader=None):
-  """Gets a dataset tuple with instructions for reading flowers.
-  Args:
-    split_name: A train/validation split name.
-    dataset_dir: The base directory of the dataset sources.
-    file_pattern: The file pattern to use when matching the dataset sources.
-      It is assumed that the pattern contains a '%s' string so that the split
-      name can be inserted.
-    reader: The TensorFlow reader type.
-  Returns:
-    A `Dataset` namedtuple.
-  Raises:
-    ValueError: if `split_name` is not a valid train/validation split.
-  """
-  if split_name not in SPLITS_TO_SIZES:
-    raise ValueError('split name %s was not recognized.' % split_name)
+    """Gets a dataset tuple with instructions for reading flowers.
+    Args:
+      split_name: A train/validation split name.
+      dataset_dir: The base directory of the dataset sources.
+      file_pattern: The file pattern to use when matching the dataset sources.
+        It is assumed that the pattern contains a '%s' string so that the split
+        name can be inserted.
+      reader: The TensorFlow reader type.
+    Returns:
+      A `Dataset` namedtuple.
+    Raises:
+      ValueError: if `split_name` is not a valid train/validation split.
+    """
+    if split_name not in SPLITS_TO_SIZES:
+        raise ValueError('split name %s was not recognized.' % split_name)
 
-  if not file_pattern:
-    file_pattern = _FILE_PATTERN
-  file_pattern = os.path.join(dataset_dir, file_pattern % split_name)
+    if not file_pattern:
+        file_pattern = _FILE_PATTERN
+    file_pattern = os.path.join(dataset_dir, file_pattern % split_name)
 
-  # Allowing None in the signature so that dataset_factory can use the default.
-  if reader is None:
-    reader = tf.TFRecordReader
-    
+    # Allowing None in the signature so that dataset_factory can use the default.
+    if reader is None:
+        reader = tf.TFRecordReader
 
-  keys_to_features = {
-      'image/encoded': tf.FixedLenFeature((), tf.string, default_value=''),
-      'image/format': tf.FixedLenFeature((), tf.string, default_value='jpg'),
-      'image/height': tf.FixedLenFeature((), tf.int64, default_value=180),
-      'image/width': tf.FixedLenFeature((), tf.int64, default_value=180),
-      'image/class/label': tf.FixedLenFeature(
-          [], tf.int64, default_value=tf.zeros([], dtype=tf.int64)),
-  }
+    keys_to_features = {
+        'image/encoded': tf.FixedLenFeature((), tf.string, default_value=''),
+        'image/format': tf.FixedLenFeature((), tf.string, default_value='jpg'),
+        'image/height': tf.FixedLenFeature((), tf.int64, default_value=180),
+        'image/width': tf.FixedLenFeature((), tf.int64, default_value=180),
+        'image/class/label': tf.FixedLenFeature(
+            [], tf.int64, default_value=tf.zeros([], dtype=tf.int64)),
+    }
 
-  items_to_handlers = {
-      'image': slim.tfexample_decoder.Image(),
-      'label': slim.tfexample_decoder.Tensor('image/class/label'),
-      'height': slim.tfexample_decoder.Tensor('image/height'),
-      'width': slim.tfexample_decoder.Tensor('image/width'),
-  }
+    items_to_handlers = {
+        'image': slim.tfexample_decoder.Image(),
+        'label': slim.tfexample_decoder.Tensor('image/class/label'),
+        'height': slim.tfexample_decoder.Tensor('image/height'),
+        'width': slim.tfexample_decoder.Tensor('image/width'),
+    }
 
-  decoder = slim.tfexample_decoder.TFExampleDecoder(
-      keys_to_features, items_to_handlers)
+    decoder = slim.tfexample_decoder.TFExampleDecoder(
+        keys_to_features, items_to_handlers)
 
-  labels_to_names = None
-  if dataset_utils.has_labels(dataset_dir):
-    labels_to_names = dataset_utils.read_label_file(dataset_dir)
+    labels_to_names = None
+    if dataset_utils.has_labels(dataset_dir):
+        labels_to_names = dataset_utils.read_label_file(dataset_dir)
 
-  return slim.dataset.Dataset(
-      data_sources=file_pattern,
-      reader=reader,
-      decoder=decoder,
-      num_samples=SPLITS_TO_SIZES[split_name],
-      items_to_descriptions=_ITEMS_TO_DESCRIPTIONS,
-      num_classes=_NUM_CLASSES,
-      labels_to_names=labels_to_names)
+    return slim.dataset.Dataset(
+        data_sources=file_pattern,
+        reader=reader,
+        decoder=decoder,
+        num_samples=SPLITS_TO_SIZES[split_name],
+        items_to_descriptions=_ITEMS_TO_DESCRIPTIONS,
+        num_classes=_NUM_CLASSES,
+        labels_to_names=labels_to_names)
